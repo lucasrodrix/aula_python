bb483b5c5825e5f30947f79b4ea5401905f066ca
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-05-23 22:11:03 -0300

    Criando Laços de Repetição

---
+++ aula3.py
@@ -0,0 +1,56 @@
+# a=int(input("Primeiro Bimestre: "))
+# if a>10:
+# 	a=int(input("Valor Não Permitido. Primeiro Bimestre: "))
+# b=int(input("Segundo Bimestre: "))
+# if a>10:
+# 	a=int(input("Valor Não Permitido. Segundo Bimestre: "))
+# c=int(input("Terceiro Bimestre: "))
+# if a>10:
+# 	a=int(input("Valor Não Permitido. Terceiro Bimestre: "))
+# d=int(input("Quarto Bimestre: "))
+# if a>10:
+# 	a=int(input("Valor Não Permitido. Quarto Bimestre: "))
+
+# media=(a+b+c+d)/4
+# print("\nMédia: {}".format(media))
+
+# if a<=10 and b<=10 and c<=10 and d<=10:
+# 	print("\nMédia: {}".format(media))
+# else:
+# 	print("\nFoi informado alguma nota errada")
+
+
+
+
+# a = int(input("Primeiro Valor: "))
+# b = int(input("Segundo Valor: "))
+# c = int(input("Terceiro Valor: "))
+
+# if a > b and a > c:
+# 	print("O maior numero é: {}".format(a))
+# elif b > a and b > c:
+# 	print("o maior numero é: {}".format(b))
+# else:
+# 	print("O maior número é: {}".format(c))
+# print("\nFinal do Programa")
+
+# a=int(input("Insira um valor: "))
+# resto = a%2
+
+# if resto == 0:
+# 	print("Numero é Par")
+# else:
+# 	print("Número é Ímpar")
+# print("\nFinal do Programa")
+
+# a = int(input("Primeiro Valor: "))
+# b = int(input("Segundo Valor: "))
+
+# mod_a = a%2
+# mod_b = b%2
+
+# if mod_a == 0 or not mod_b ==0:
+# 	print("O Numero digitado é Par")
+# else:
+# 	print("Nenhum numero digitado é par")
+# print("\nFim do Programa")
---
+++ aula4.py
@@ -0,0 +1,52 @@
+resultado = 0
+for x in range(1, 10):
+	if x < 9:
+		resultado += 1
+print(resultado)
+
+
+# a=int(input("Primeiro Bimestre: "))
+# while a>10:
+# 	a=int(input("Valor Não Permitido. Primeiro Bimestre: "))
+# b=int(input("Segundo Bimestre: "))
+# while b>10:
+# 	a=int(input("Valor Não Permitido. Segundo Bimestre: "))
+# c=int(input("Terceiro Bimestre: "))
+# while c>10:
+# 	a=int(input("Valor Não Permitido. Terceiro Bimestre: "))
+# d=int(input("Quarto Bimestre: "))
+# while d>10:
+# 	a=int(input("Valor Não Permitido. Quarto Bimestre: "))
+
+# media=(a+b+c+d)/4
+# print("\nMédia: {}".format(media))
+
+# a = 0
+# while a <= 10:
+# 	print(a)
+# 	a += 1
+
+# div =0
+
+# a=int(input("Entre com um valor: "))
+# for x in range(1,a+1):
+# 	 resto = a%x
+# 	 if resto == 0:
+# 	 	div += 1
+
+# if div == 2:
+# 	print('Numero {} é primo'.format(a))
+# else:
+# 	print('Numero {} não é primo'.format(a))
+
+# for num in range(101):
+# 	div =0
+# 	for x in range(1, num+1):
+# 		 resto = num%x
+# 		 if resto == 0:
+# 		 	div += 1
+
+# 	if div == 2:
+# 		print(num)
+# 	# else:
+# 	# 	print('Numero {} não é primo'.format(a))
